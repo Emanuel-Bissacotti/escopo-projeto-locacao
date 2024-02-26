7466e4362275896efa98b198ba4354e9edf8be72
Emanuel-Bissacotti <emanuelfbissacotti@gmail.com>
2024-02-26 19:20:42 -0300

    Adiciona escopo todo
    Precisa redefinir centro do mapa quando o usuario
    clica em determinada linha.

---
+++ script.js
@@ -0,0 +1,676 @@
+const jsonData = [
+    {
+        "rentID": 500,
+        "frota": 26,
+        "model": "T7-260",
+        "rentData": "2023-09-18",
+        "lastEndDate": "2024-03-18",
+        "coordinates": "-14.14296627,-54.28299665",
+        "clienteName": "AGUA SANTA AGROINDUSTRIAL LTDA"
+    },
+    {
+        "rentID": 501,
+        "frota": 27,
+        "model": "T7-260",
+        "rentData": "2023-09-18",
+        "lastEndDate": "2024-03-18",
+        "coordinates": null,
+        "clienteName": "AGUA SANTA AGROINDUSTRIAL LTDA"
+    },
+    {
+        "rentID": 543,
+        "frota": 118,
+        "model": "T7-240",
+        "rentData": "2024-01-11",
+        "lastEndDate": "2024-03-11",
+        "coordinates": null,
+        "clienteName": "ARTHEMIO OLEGARIO DE SOUZA JUNIOR"
+    },
+    {
+        "rentID": 544,
+        "frota": 119,
+        "model": "T7-240",
+        "rentData": "2024-01-11",
+        "lastEndDate": "2024-03-11",
+        "coordinates": null,
+        "clienteName": "ARTHEMIO OLEGARIO DE SOUZA JUNIOR"
+    },
+    {
+        "rentID": 545,
+        "frota": 101,
+        "model": "T7-240",
+        "rentData": "2024-01-11",
+        "lastEndDate": "2024-03-11",
+        "coordinates": null,
+        "clienteName": "ARTHEMIO OLEGARIO DE SOUZA JUNIOR"
+    },
+    {
+        "rentID": 546,
+        "frota": 102,
+        "model": "T7-240",
+        "rentData": "2024-01-11",
+        "lastEndDate": "2024-03-11",
+        "coordinates": "-21.56835079,-55.47243833",
+        "clienteName": "ARTHEMIO OLEGARIO DE SOUZA JUNIOR"
+    },
+    {
+        "rentID": 547,
+        "frota": 103,
+        "model": "T7-240",
+        "rentData": "2024-01-12",
+        "lastEndDate": "2024-03-20",
+        "coordinates": "-23.75854611,-54.90205049",
+        "clienteName": "JEAN MICHAEL WEBER"
+    },
+    {
+        "rentID": 549,
+        "frota": 104,
+        "model": "T7-240",
+        "rentData": "2024-01-15",
+        "lastEndDate": "2024-03-15",
+        "coordinates": "-23.76069188,-54.89576339",
+        "clienteName": "JEAN MICHAEL WEBER"
+    },
+    {
+        "rentID": 550,
+        "frota": 99,
+        "model": "T7-240",
+        "rentData": "NaN-NaN-NaN",
+        "lastEndDate": "2024-03-16",
+        "coordinates": "-21.51650905,-55.43209791",
+        "clienteName": "ARTHEMIO OLEGARIO DE SOUZA JUNIOR"
+    },
+    {
+        "rentID": 551,
+        "frota": 100,
+        "model": "T7-240",
+        "rentData": "2024-01-16",
+        "lastEndDate": "2024-03-16",
+        "coordinates": "-21.24707579,-55.04774808",
+        "clienteName": "ARTHEMIO OLEGARIO DE SOUZA JUNIOR"
+    },
+    {
+        "rentID": 552,
+        "frota": 109,
+        "model": "T7-240",
+        "rentData": "NaN-NaN-NaN",
+        "lastEndDate": "2024-03-16",
+        "coordinates": "-21.28824234,-55.38484811",
+        "clienteName": "ARTHEMIO OLEGARIO DE SOUZA JUNIOR"
+    },
+    {
+        "rentID": 553,
+        "frota": 110,
+        "model": "T7-240",
+        "rentData": "NaN-NaN-NaN",
+        "lastEndDate": "2024-03-16",
+        "coordinates": "-21.28869295,-55.3842473",
+        "clienteName": "ARTHEMIO OLEGARIO DE SOUZA JUNIOR"
+    },
+    {
+        "rentID": 554,
+        "frota": 97,
+        "model": "T7-240",
+        "rentData": "2024-01-17",
+        "lastEndDate": "2024-03-17",
+        "coordinates": "-21.28836035,-55.38489103",
+        "clienteName": "ARTHEMIO OLEGARIO DE SOUZA JUNIOR"
+    },
+    {
+        "rentID": 555,
+        "frota": 98,
+        "model": "T7-240",
+        "rentData": "2024-01-17",
+        "lastEndDate": "2024-03-17",
+        "coordinates": null,
+        "clienteName": "ARTHEMIO OLEGARIO DE SOUZA JUNIOR"
+    },
+    {
+        "rentID": 556,
+        "frota": 1,
+        "model": "T7-260",
+        "rentData": "2024-01-18",
+        "lastEndDate": "2024-03-18",
+        "coordinates": "-21.50580167,-55.44853448",
+        "clienteName": "ARTHEMIO OLEGARIO DE SOUZA JUNIOR"
+    },
+    {
+        "rentID": 558,
+        "frota": 112,
+        "model": "T7-240",
+        "rentData": "2024-02-19",
+        "lastEndDate": "2024-03-19",
+        "coordinates": "-20.74982643,-55.36667346",
+        "clienteName": "LUCAS DA ROCHA"
+    },
+    {
+        "rentID": 559,
+        "frota": 114,
+        "model": "T7-240",
+        "rentData": "2024-01-24",
+        "lastEndDate": "2024-03-24",
+        "coordinates": "-21.68974757,-55.11508226",
+        "clienteName": "LUCAS DA ROCHA"
+    },
+    {
+        "rentID": 560,
+        "frota": 115,
+        "model": "T7-240",
+        "rentData": "2024-01-24",
+        "lastEndDate": "2024-03-24",
+        "coordinates": null,
+        "clienteName": "LUCAS DA ROCHA"
+    },
+    {
+        "rentID": 561,
+        "frota": 203,
+        "model": "T8-385",
+        "rentData": "2024-01-25",
+        "lastEndDate": "2024-02-25",
+        "coordinates": "-22.21114754,-54.71702098",
+        "clienteName": "LUCAS DA ROCHA"
+    },
+    {
+        "rentID": 562,
+        "frota": 20,
+        "model": "T7-260",
+        "rentData": "2024-01-25",
+        "lastEndDate": "2024-03-25",
+        "coordinates": "-21.68945789,-55.11572599",
+        "clienteName": "LUCAS DA ROCHA"
+    },
+    {
+        "rentID": 563,
+        "frota": 24,
+        "model": "T7-260",
+        "rentData": "2024-01-29",
+        "lastEndDate": "2024-02-28",
+        "coordinates": "-21.76243543,-55.47288894",
+        "clienteName": "SEBASTIAAN SIMON PETRUS SPEKKEN"
+    },
+    {
+        "rentID": 564,
+        "frota": 107,
+        "model": "T7-240",
+        "rentData": "2024-02-01",
+        "lastEndDate": "2024-03-02",
+        "coordinates": "-21.61934494,-55.13126134",
+        "clienteName": "DIRCEU LUIZ BROCH"
+    },
+    {
+        "rentID": 565,
+        "frota": 91,
+        "model": "T7-240",
+        "rentData": "2024-01-30",
+        "lastEndDate": "2024-02-29",
+        "coordinates": "-21.77473068,-55.41377305",
+        "clienteName": "SEBASTIAAN SIMON PETRUS SPEKKEN"
+    },
+    {
+        "rentID": 566,
+        "frota": 111,
+        "model": "T7-240",
+        "rentData": "2024-02-06",
+        "lastEndDate": "2024-02-29",
+        "coordinates": "-21.76864743,-55.07319688",
+        "clienteName": "RAFAEL PONTE SARIAN"
+    },
+    {
+        "rentID": 567,
+        "frota": 18,
+        "model": "T7-260",
+        "rentData": "2024-02-02",
+        "lastEndDate": "2024-03-02",
+        "coordinates": "-22.21140503,-54.71708536",
+        "clienteName": "CARLOS EDUARDO TRUITE MENDES"
+    },
+    {
+        "rentID": 570,
+        "frota": 15,
+        "model": "T7-260",
+        "rentData": "2024-02-02",
+        "lastEndDate": "2024-03-02",
+        "coordinates": "-22.00111985,-54.31610584",
+        "clienteName": "MATHEUS CACERES COFFERI"
+    },
+    {
+        "rentID": 571,
+        "frota": 16,
+        "model": "T7-260",
+        "rentData": "2024-02-02",
+        "lastEndDate": "2024-03-02",
+        "coordinates": null,
+        "clienteName": "MATHEUS CACERES COFFERI"
+    },
+    {
+        "rentID": 572,
+        "frota": 17,
+        "model": "T7-260",
+        "rentData": "2024-02-02",
+        "lastEndDate": "2024-03-02",
+        "coordinates": "-22.08700418,-54.20341014",
+        "clienteName": "MATHEUS CACERES COFFERI"
+    },
+    {
+        "rentID": 573,
+        "frota": 95,
+        "model": "T7-240",
+        "rentData": "2024-02-02",
+        "lastEndDate": "2024-02-29",
+        "coordinates": "-21.41611933,-54.77133035",
+        "clienteName": "SEBASTIAAN SIMON PETRUS SPEKKEN"
+    },
+    {
+        "rentID": 575,
+        "frota": 6,
+        "model": "T7-260",
+        "rentData": "2024-02-03",
+        "lastEndDate": "2024-03-03",
+        "coordinates": null,
+        "clienteName": "PETER FERTER"
+    },
+    {
+        "rentID": 577,
+        "frota": 92,
+        "model": "T7-240",
+        "rentData": "2024-02-03",
+        "lastEndDate": "2024-02-29",
+        "coordinates": null,
+        "clienteName": "SEBASTIAAN SIMON PETRUS SPEKKEN"
+    },
+    {
+        "rentID": 578,
+        "frota": 11,
+        "model": "T7-260",
+        "rentData": "2024-02-02",
+        "lastEndDate": "2024-03-03",
+        "coordinates": "-13.05818438,-55.93944311",
+        "clienteName": "JORGE LUIZ BRIZOT"
+    },
+    {
+        "rentID": 579,
+        "frota": 10,
+        "model": "T7-260",
+        "rentData": "2024-02-02",
+        "lastEndDate": "2024-03-03",
+        "coordinates": "-12.77697086,-56.62441492",
+        "clienteName": "EDSON FERNANDO SCARSI"
+    },
+    {
+        "rentID": 580,
+        "frota": 2,
+        "model": "T7-260",
+        "rentData": "2024-02-03",
+        "lastEndDate": "2024-03-08",
+        "coordinates": "-21.20138168,-55.28543472",
+        "clienteName": "JAN DE BAAR KREPEL"
+    },
+    {
+        "rentID": 582,
+        "frota": 108,
+        "model": "T7-240",
+        "rentData": "2024-02-05",
+        "lastEndDate": "2024-03-05",
+        "coordinates": "-22.35450625,-54.94048118",
+        "clienteName": "CASSIO MEDEIROS CORRÊA"
+    },
+    {
+        "rentID": 583,
+        "frota": 90,
+        "model": "T7-240",
+        "rentData": "2023-02-05",
+        "lastEndDate": "2024-03-06",
+        "coordinates": null,
+        "clienteName": "MELITOM DA SILVA"
+    },
+    {
+        "rentID": 584,
+        "frota": 96,
+        "model": "T7-240",
+        "rentData": "2024-02-05",
+        "lastEndDate": "2024-03-06",
+        "coordinates": "-21.56331896,-55.35382032",
+        "clienteName": "Eduardo Luiz Heck"
+    },
+    {
+        "rentID": 585,
+        "frota": 113,
+        "model": "T7-240",
+        "rentData": "2024-02-09",
+        "lastEndDate": "2024-03-05",
+        "coordinates": "-21.54337406,-55.74795484",
+        "clienteName": "Saulo André Comparim Ferreira"
+    },
+    {
+        "rentID": 586,
+        "frota": 83,
+        "model": "T7-240",
+        "rentData": "2024-02-07",
+        "lastEndDate": "2023-03-06",
+        "coordinates": "-21.55691385,-55.71126222",
+        "clienteName": "ASTOR PAULO HECK"
+    },
+    {
+        "rentID": 587,
+        "frota": 79,
+        "model": "T7-240",
+        "rentData": "2024-02-09",
+        "lastEndDate": "2024-03-08",
+        "coordinates": "-22.13149666,-55.82861423",
+        "clienteName": "FABIO CUNHA FERNANDES"
+    },
+    {
+        "rentID": 588,
+        "frota": 84,
+        "model": "T7-240",
+        "rentData": "2024-02-09",
+        "lastEndDate": "2024-03-06",
+        "coordinates": null,
+        "clienteName": "CASSIO MEDEIROS CORRÊA"
+    },
+    {
+        "rentID": 589,
+        "frota": 86,
+        "model": "T7-240",
+        "rentData": "2024-02-09",
+        "lastEndDate": "2024-03-06",
+        "coordinates": "-21.8891859,-55.47666549",
+        "clienteName": "CASSIO MEDEIROS CORRÊA"
+    },
+    {
+        "rentID": 590,
+        "frota": 87,
+        "model": "T7-240",
+        "rentData": "2024-02-09",
+        "lastEndDate": "2024-03-06",
+        "coordinates": "-21.91527843,-55.499475",
+        "clienteName": "CASSIO MEDEIROS CORRÊA"
+    },
+    {
+        "rentID": 591,
+        "frota": 19,
+        "model": "T7-260",
+        "rentData": "2024-02-07",
+        "lastEndDate": "2024-03-07",
+        "coordinates": null,
+        "clienteName": "ANTONIO DE MORAES RIBEIRO NETO"
+    },
+    {
+        "rentID": 592,
+        "frota": 21,
+        "model": "T7-260",
+        "rentData": "2024-02-07",
+        "lastEndDate": "2024-03-07",
+        "coordinates": "-21.3921833,-55.33686876",
+        "clienteName": "ANTONIO DE MORAES RIBEIRO NETO"
+    },
+    {
+        "rentID": 593,
+        "frota": 116,
+        "model": "T7-240",
+        "rentData": "2024-02-08",
+        "lastEndDate": "2024-03-08",
+        "coordinates": null,
+        "clienteName": "JOAO FERNANDO TISOTT"
+    },
+    {
+        "rentID": 594,
+        "frota": 70,
+        "model": "T7-240",
+        "rentData": "2024-02-08",
+        "lastEndDate": "2024-03-08",
+        "coordinates": "-21.65890216,-55.96319675",
+        "clienteName": "ROBERTO DE OLIVEIRA"
+    },
+    {
+        "rentID": 595,
+        "frota": 80,
+        "model": "T7-240",
+        "rentData": "2024-02-08",
+        "lastEndDate": "2024-03-08",
+        "coordinates": "-21.54120683,-55.14252662",
+        "clienteName": "ROBERTO DE OLIVEIRA"
+    },
+    {
+        "rentID": 596,
+        "frota": 81,
+        "model": "T7-240",
+        "rentData": "2024-02-08",
+        "lastEndDate": "2024-03-08",
+        "coordinates": null,
+        "clienteName": "ROBERTO DE OLIVEIRA"
+    },
+    {
+        "rentID": 597,
+        "frota": 14,
+        "model": "T7-260",
+        "rentData": "2024-02-06",
+        "lastEndDate": "2024-03-07",
+        "coordinates": "-21.9779241,-55.84872007",
+        "clienteName": "RAMÃO SANABRIA"
+    },
+    {
+        "rentID": 599,
+        "frota": 5,
+        "model": "T7-260",
+        "rentData": "2024-02-14",
+        "lastEndDate": "2024-03-14",
+        "coordinates": null,
+        "clienteName": "ALEXANDRE LANZONI"
+    },
+    {
+        "rentID": 600,
+        "frota": 106,
+        "model": "T7-240",
+        "rentData": "2024-02-07",
+        "lastEndDate": "2024-03-08",
+        "coordinates": "-21.78555607,-55.45997142",
+        "clienteName": "FELIPE BELIAN ALVES"
+    },
+    {
+        "rentID": 601,
+        "frota": 12,
+        "model": "T7-260",
+        "rentData": "2024-02-07",
+        "lastEndDate": "2024-03-07",
+        "coordinates": null,
+        "clienteName": "CARLOS EDUARDO TRUITE MENDES"
+    },
+    {
+        "rentID": 602,
+        "frota": 71,
+        "model": "T7-240",
+        "rentData": "2024-02-08",
+        "lastEndDate": "2024-03-08",
+        "coordinates": null,
+        "clienteName": "ROBERTO DE OLIVEIRA"
+    },
+    {
+        "rentID": 603,
+        "frota": 72,
+        "model": "T7-240",
+        "rentData": "2024-02-08",
+        "lastEndDate": "2024-03-08",
+        "coordinates": null,
+        "clienteName": "ROBERTO DE OLIVEIRA"
+    },
+    {
+        "rentID": 604,
+        "frota": 77,
+        "model": "T7-240",
+        "rentData": "2024-02-08",
+        "lastEndDate": "2024-03-08",
+        "coordinates": null,
+        "clienteName": "ROBERTO DE OLIVEIRA"
+    },
+    {
+        "rentID": 605,
+        "frota": 78,
+        "model": "T7-240",
+        "rentData": "2024-02-08",
+        "lastEndDate": "2024-03-08",
+        "coordinates": null,
+        "clienteName": "ROBERTO DE OLIVEIRA"
+    },
+    {
+        "rentID": 606,
+        "frota": 117,
+        "model": "T7-240",
+        "rentData": "2024-02-14",
+        "lastEndDate": "2024-03-09",
+        "coordinates": "-21.67494177,-55.6810069",
+        "clienteName": "MICHAEL GIANLUPI "
+    },
+    {
+        "rentID": 607,
+        "frota": 74,
+        "model": "T7-240",
+        "rentData": "2024-02-11",
+        "lastEndDate": "2024-03-12",
+        "coordinates": "-21.95023298,-55.53241252",
+        "clienteName": "REGINA FATIMA ALVES CORRÊA IGLESIAS"
+    },
+    {
+        "rentID": 608,
+        "frota": 75,
+        "model": "T7-240",
+        "rentData": "2024-02-11",
+        "lastEndDate": "2024-03-02",
+        "coordinates": "-21.95032954,-55.53241252",
+        "clienteName": "REGINA FATIMA ALVES CORRÊA IGLESIAS"
+    },
+    {
+        "rentID": 609,
+        "frota": 76,
+        "model": "T7-240",
+        "rentData": "2024-02-11",
+        "lastEndDate": "2024-03-11",
+        "coordinates": "-21.81583285,-55.09094238",
+        "clienteName": "REGINA FATIMA ALVES CORRÊA IGLESIAS"
+    },
+    {
+        "rentID": 610,
+        "frota": 25,
+        "model": "T7-260",
+        "rentData": "2024-02-11",
+        "lastEndDate": "NaN-NaN-NaN",
+        "coordinates": null,
+        "clienteName": "JULIANO CESAR ADAMS"
+    },
+    {
+        "rentID": 612,
+        "frota": 201,
+        "model": "T8-385",
+        "rentData": "2024-02-12",
+        "lastEndDate": "2024-03-12",
+        "coordinates": null,
+        "clienteName": "BRENO DE ARRUDA MORAES RIBEIRO"
+    },
+    {
+        "rentID": 613,
+        "frota": 94,
+        "model": "T7-240",
+        "rentData": "2024-02-12",
+        "lastEndDate": "2024-03-13",
+        "coordinates": "-21.40387773,-55.31886577",
+        "clienteName": "ANDRÉ DE ARRUDA MORAES RIBEIRO"
+    },
+    {
+        "rentID": 614,
+        "frota": 204,
+        "model": "T8-440",
+        "rentData": "2024-02-15",
+        "lastEndDate": "2024-03-15",
+        "coordinates": "-20.75616717,-55.37188768",
+        "clienteName": "LUCAS DA ROCHA"
+    },
+    {
+        "rentID": 615,
+        "frota": 205,
+        "model": "T8-440",
+        "rentData": "2024-02-19",
+        "lastEndDate": "2024-03-20",
+        "coordinates": "-21.99923157,-54.32136297",
+        "clienteName": "MATHEUS CACERES COFFERI"
+    },
+    {
+        "rentID": 616,
+        "frota": 82,
+        "model": "T7-240",
+        "rentData": "2024-02-23",
+        "lastEndDate": "2024-03-23",
+        "coordinates": null,
+        "clienteName": "ADOLFO VLADIMIR ABREU BARRIOS"
+    }
+]
+
+function table() {
+    let htmlTable = "<table>";
+
+
+    htmlTable += "<tr>";
+
+    htmlTable += "<td>ID </td>";
+    htmlTable += "<td>Frota </td>";
+    htmlTable += "<td>Model </td>";
+    htmlTable += "<td>Data do aluguel </td>";
+    htmlTable += "<td>Data de devolução </td>";
+    htmlTable += "<td>Coordenadas </td>";
+    htmlTable += "<td>Nome do cliente </td>";
+    htmlTable += "</tr>";
+
+    jsonData.forEach(item => {
+        htmlTable += "<tr>";
+        for (let key in item) {
+            htmlTable += `<td>${item[key]}</td>`;
+        }
+        htmlTable += "</tr>";
+    });
+
+    htmlTable += "</table>";
+
+    document.getElementById("tableContainer").innerHTML = htmlTable;
+}
+
+table();
+
+async function initMap() {
+    const { Map } = await google.maps.importLibrary("maps");
+    let map;
+
+    map = new Map(document.getElementById("map"), {
+            center: { lat: -18.1533, lng: -50.8080 },
+            zoom: 5,
+        });
+
+    jsonData.forEach(item => {
+        if(item.coordinates === null){
+            return
+        }
+        let coordinates = item.coordinates.split(",");
+        let lat = parseFloat(coordinates[0]);
+        let lng = parseFloat(coordinates[1]);
+        new google.maps.Marker({
+            position: { lat, lng },
+            map: map,
+            icon: {
+                url: "assets/trator.png",
+                scaledSize: new google.maps.Size(20, 20)
+            }
+        });
+    });
+}
+
+let tableRows = document.querySelectorAll("#tableContainer tr");
+tableRows.forEach(row => {
+    row.addEventListener("click", function() {
+        console.log(row.querySelector("td:nth-child(6)").textContent);
+        if(row.querySelector("td:nth-child(6)").textContent !== null){
+            let coordinate = row.querySelector("td:nth-child(6)").textContent.split(",");
+            let lat = -34.397;
+            let lng = 150.644;
+            map.setCenter({ lat, lng });
+            console.log("teste");
+        }
+    });
+});
